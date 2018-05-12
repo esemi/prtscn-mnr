3e83a976b4c438f1769ce8df4062599c5bf4e2b1
Simon <spam@esemi.ru>
2018-05-12 17:28:54 +0300

    :sparkles: Increase task speed

--- app/settings.py
+++ app/settings.py
@@ -1,15 +1,13 @@
 import logging
 
-MINER_KEYS = {
-    'thumba_dummy': 'nA8P6IYnM3KluYUc7geYgUGObEERrGQF',
-}
-
 DST_URL_TEMPLATE = "https://prtscnmnr.esemi.ru/mnr.html?page=%s&user=%s"
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
 
 THUMBA_HOST = 'https://www.thumbalizr.com/demo'
 THUMBA_QUERY_LIMIT = 50
+
 SCREENSHOTMACHINE_HOST = 'https://www.screenshotmachine.com/processor.php'
 SCREENSHOTMACHINE_QUERY_LIMIT = 50
+SCREENSHOTMACHINE_CONCURRENCY = 5
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
--- app/task_screenshotmachine.py
+++ app/task_screenshotmachine.py
@@ -1,17 +1,31 @@
 import logging
 import uuid
+import asyncio
 
-import requests
+import aiohttp
 
-from settings import DST_URL_TEMPLATE, SCREENSHOTMACHINE_QUERY_LIMIT, SCREENSHOTMACHINE_HOST, USER_AGENT
+from settings import DST_URL_TEMPLATE, SCREENSHOTMACHINE_QUERY_LIMIT, SCREENSHOTMACHINE_HOST, USER_AGENT, \
+    SCREENSHOTMACHINE_CONCURRENCY
+
+
+async def task(num, sem: asyncio.Semaphore):
+    url = DST_URL_TEMPLATE % (uuid.uuid4().hex, 'screenshotmachine')
+    async with sem:
+        async with aiohttp.ClientSession(headers={'User-Agent': USER_AGENT}, raise_for_status=True) as session:
+            try:
+                async with session.post(SCREENSHOTMACHINE_HOST, data={'urlparam': url, 'device': 'desktop'}) as response:
+                    resp = await response.text()
+                    logging.info('%s: %s %s', num, url, resp[:100])
+            except Exception as e:
+                logging.info('exception %s', e)
+
+
+async def main():
+    sem = asyncio.Semaphore(SCREENSHOTMACHINE_CONCURRENCY)
+    tasks = [asyncio.ensure_future(task(i, sem)) for i in range(SCREENSHOTMACHINE_QUERY_LIMIT)]
+    await asyncio.wait(tasks)
+
 
 if __name__ == '__main__':
-    for i in range(SCREENSHOTMACHINE_QUERY_LIMIT):
-        url = DST_URL_TEMPLATE % (uuid.uuid4().hex, 'screenshotmachine')
-        try:
-            resp = requests.post(SCREENSHOTMACHINE_HOST, data={'urlparam': url, 'device': 'desktop'}, headers={
-                'User-Agent': USER_AGENT})
-        except Exception as e:
-            logging.info('exception %s', e)
-        else:
-            logging.info('%s: %s %s %s', i, url, resp.status_code, resp.text)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
